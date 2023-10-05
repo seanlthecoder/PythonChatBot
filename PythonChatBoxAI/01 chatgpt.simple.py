b664a8c42f2598d1bfbca3d3fb255c6d62098ee9
Sean Lester Durham <sean.durham1982@gmail.com>
2023-10-05 06:12:36 -0500

    added knowledge base

--- PythonChatBoxAI/01 chatgpt.simple.py
+++
@@ -1,8 +0,0 @@
-import openai
-
-openai.api_key = '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'
-
-completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Give me 3 ideas for apps I could buikld with openai apis "}])
-
-print(completion.choices[0].message.content)
-
